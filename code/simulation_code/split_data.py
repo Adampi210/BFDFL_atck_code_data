61e04d5c9bdd65c12a1c2a2e445b6517721bb58c
root <root@apiasecz.1.1.1.1>
2023-06-02 18:46:25 +0000

    Finished non-iid implementation + different dsets

--- code/simulation_code/split_data.py
+++ code/simulation_code/split_data.py
@@ -12,6 +12,7 @@ from torch.utils.data import DataLoader
 
 import random
 import numpy as np
+
 # Set manual seeds for reproducibility 
 random.seed(0)
 torch.manual_seed(0)
@@ -19,8 +20,8 @@ np.random.seed(0)
 
 # Number of classes each non-iid client should have
 NUM_CLASSES_NONIID = 3
-TOTAL_CLASSES = 10
-# Split data between servers and clients IID
+
+# Split data between servers and clients iid
 def split_data_iid_incl_server(num_servers, num_clients, dataset_name):
     # Data directory
     data_dir = '~/data/datasets' + dataset_name
@@ -62,8 +63,7 @@ def split_data_iid_incl_server(num_servers, num_clients, dataset_name):
                             for subset in valid_data_split]
     return train_list_dsets, valid_list_dsets
 
-
-# This function only splits the data between the clients, currently split is iid
+# Split data betweeen clients iid
 def split_data_iid_excl_server(num_clients, dataset_name):
     # Data directory
     data_dir = '~/data/datasets' + dataset_name
@@ -104,11 +104,87 @@ def split_data_iid_excl_server(num_clients, dataset_name):
 
     return train_list_dsets, valid_list_dsets
 
+# Splits the dataset in a non-iid way
+def non_iid_split(data_set, num_clients):
+    # Calculate number of classes
+    num_classes = len(data_set.classes)
+    # Split the classes among clients
+    client_classes = [random.sample(list(range(num_classes)), NUM_CLASSES_NONIID) for _ in range(num_clients)]
+    # For each class, calculate how many clients use it
+    class_usage = {i:0 for i in range(num_classes)}
+    # Iterate through client classes to do that
+    for client_class_list in client_classes:
+        for client_class in client_class_list:
+            class_usage[client_class] += 1
+    # Next, separate the data for differen classes, and shuffle it
+    data_np, labels_np = np.array(data_set.data[:]), np.array(data_set.targets[:])
 
+    # Create a dict that holds the datapoints for every class
+    # I.e. for every class it will hold n separate lists, where n is the number of times the class is used
+    data_per_class_dict = {class_data: None for class_data in class_usage.keys()}
+    # First, split the data per class
+    for class_data in data_per_class_dict.keys():
+        # For each class, separate only the values whose label is equal to the current class
+        data_per_class_dict[class_data] = data_np[labels_np[:] == class_data]
+        # Also random shuffle to make sure this is non-biased
+        random.shuffle(data_per_class_dict[class_data])
+    
+    # Then iterate again, but for each class data, split it into specific number of arrays
+    for class_data in data_per_class_dict.keys():
+        # The number of arrays to split into is determined by how many clients use this class
+        data_per_class_dict[class_data] = np.array_split(data_per_class_dict[class_data], class_usage[class_data])
+
+    # Iterator will be used to keep track of how many times a given class was distributed
+    iterator_per_class = {class_data: 0 for class_data in class_usage.keys()}
+    # List of Tensor datasets for every client (i.e. the dataset for every client)
+    list_dsets = []
+
+    # For every client iterate through its classes
+    for client_class_list in client_classes:
+        client_data = []   # Will hold the data the client gets
+        client_labels = [] # Will hold the labels the client gets
+        # Then go through each class
+        for client_class in client_class_list:
+            # Extend the client data by the data corresponding to the class
+            client_data.extend(data_per_class_dict[client_class][iterator_per_class[client_class]])
+            # And extend the labels by the labels of that class
+            client_labels.extend([client_class] * len(data_per_class_dict[client_class][iterator_per_class[client_class]]))
+            # Increment iterator since a class was used up
+            iterator_per_class[client_class] += 1
+        # Convert both to numpy arrays
+        client_data = np.array(client_data)
+        client_labels = np.array(client_labels)
+        # Add the client data to the list of datasets
+        list_dsets.append(data_utils.TensorDataset(torch.tensor(client_data), torch.tensor(client_labels)))
+    return list_dsets
+
+# Split data between servers and clients non-iid
 def split_data_non_iid_incl_server(num_servers, num_clients, dataset_name):
-    # TODO
-    pass
+    # Data directory
+    data_dir = '~/data/datasets' + dataset_name
+    # Decide on number of total data containers (chunks of data to split into)
+    total_data_containers = num_clients + num_servers
+    # Get the data, currently 3 datasets available
+    if dataset_name == 'mnist':
+        train_data = torchvision.datasets.MNIST(root = data_dir, train = True, download = True, transform = transforms.ToTensor())
+        validation_data  = torchvision.datasets.MNIST(root = data_dir, train = False, transform = transforms.ToTensor())
+    elif dataset_name == 'fmnist':
+        train_data = torchvision.datasets.FashionMNIST(root = data_dir, train = True, download = True, transform = transforms.ToTensor())
+        validation_data  = torchvision.datasets.FashionMNIST(root = data_dir, train = False, transform = transforms.ToTensor())
+    elif dataset_name == 'cifar10':
+        train_data = torchvision.datasets.CIFAR10(root = data_dir, train = True, download = True, transform = transforms.ToTensor())
+        validation_data  = torchvision.datasets.CIFAR10(root = data_dir, train = False, transform = transforms.ToTensor())
+    else:
+        print(f'Currently {dataset_name} is not supported')
+        return -1
+    # Split the training and validation data
+    train_list_dsets = non_iid_split(train_data, total_data_containers)
+    valid_list_dsets = non_iid_split(validation_data, total_data_containers)
 
+    # Return the split datasets
+    return train_list_dsets, valid_list_dsets
+
+# Split data betweeen clients non-iid
 def split_data_non_iid_excl_server(num_clients, dataset_name):
     # Data directory
     data_dir = '~/data/datasets' + dataset_name
@@ -125,52 +201,9 @@ def split_data_non_iid_excl_server(num_clients, dataset_name):
     else:
         print(f'Currently {dataset_name} is not supported')
         return -1
-    # Get the fractions that determine the split (i.e. what fraction of the dataset each split will have)
-    data_split_fractions = [random.random() for i in range(num_clients)]
-    # Make sure the fractions sum up to 1
-    sum_fractions = sum(data_split_fractions)
-    while sum_fractions != 1:
-        data_split_fractions = [i / sum(data_split_fractions) for i in data_split_fractions]
-        sum_fractions = sum(data_split_fractions)
+    # Split the training and validation data
+    train_list_dsets = non_iid_split(train_data, num_clients)
+    valid_list_dsets = non_iid_split(validation_data, num_clients)
 
-    # Split the classes among clients
-    client_classes = [random.sample(list(range(TOTAL_CLASSES)), NUM_CLASSES_NONIID) for _ in range(num_clients)]
-    # Calculate data-set sizes for each client based on their fraction sizes (train and validation data)
-    train_per_client, valid_per_client = [int(len(train_data) * fraction) for fraction in data_split_fractions], [int(len(validation_data) * fraction) for fraction in data_split_fractions]
-    train_per_client[-1] = len(train_data) - sum(train_per_client[0:-1])
-    valid_per_client[-1] = len(validation_data) - sum(valid_per_client[0:-1])
-    # Calculate number of data-points per client per each non-iid class
-    train_per_class_per_client = [{client_class:int(train_per_client[i] / 3) if j != NUM_CLASSES_NONIID - 1 
-        else 0 for j, client_class in enumerate(client_class_list)} for i, client_class_list in enumerate(client_classes)]
-    valid_per_class_per_client = [{client_class:int(valid_per_client[i] / 3) if j != NUM_CLASSES_NONIID - 1 
-        else 0 for j, client_class in enumerate(client_class_list)} for i, client_class_list in enumerate(client_classes)]
-    # Adjust for the floating-point to int conversion
-    for i in range(len(train_per_class_per_client)):
-        train_per_class_per_client[i][client_classes[i][NUM_CLASSES_NONIID - 1]] = train_per_client[i] - sum(train_per_class_per_client[i].values())
-        valid_per_class_per_client[i][client_classes[i][NUM_CLASSES_NONIID - 1]] = valid_per_client[i] - sum(valid_per_class_per_client[i].values())
-    # There's a problem here:
-    client_sums = {i: 0 for i in range(10)}
-    for client_class_list in train_per_class_per_client:
-        for client_class in client_class_list.keys():
-            client_sums[client_class] += client_class_list[client_class]
-    # TODO: Fix idea -> first calculate how to split the classes among clients given available data points for each client
-    # Try to do that with fixed numbers first. So for example, we know that 6000 0's, and if 4 clients use it, then 1500 each
-    # It might be easier to do it that way: choose random, then split uniformly per client, so the choice determines the dataset size
-    # Friday: implement that + FCN (MLP) for centralized FMNIST + CNN for CIFAR10 + attacks centralized. Then can run some simulations finally
-    # When sims are going: Finish writing proofs in notebook + Start literature review + look over other proofs
-    # Also implement decentralized + look at centrality and attacks
-    print(client_sums)
-    print(len(train_data), sum(client_sums.values()))
-    # Finally, split the data accordingly
-    
-
-    return [], []
-
-# Use FMNIST 
-# Use 50 devices
-# Decentralized + 1 adversarial + consider centrality + undirected + DFedAvg (assume matrix to be doubly stochastic)
-# Add non-iid
-servers, clients = 1, 10
-dset_len = clients
-a,b = split_data_non_iid_excl_server(clients, 'fmnist')
-
+    # Return the split datasets
+    return train_list_dsets, valid_list_dsets
