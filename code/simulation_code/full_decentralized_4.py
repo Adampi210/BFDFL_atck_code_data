a5b3930b92ee7d81b9bbfdd2a21bacc537c6159f
root <root@apiasecz.1.1.1.1>
2023-07-17 22:08:02 +0000

    added env file

--- code/simulation_code/full_decentralized_4.py
+++ code/simulation_code/full_decentralized_4.py
@@ -34,7 +34,7 @@ if device_used != torch.device('cuda:2'):
 
 start_time = time.time()
 # Set hyperparameters
-seed = 24 # Seed for PRNGs 
+seed = 25 # Seed for PRNGs 
 random.seed(seed)
 torch.manual_seed(seed)
 np.random.seed(seed)
@@ -128,7 +128,7 @@ nb_iter = 15   # Number of epochs for PGD attack
 
 # Define centrality measures and directories
 centralities = ('none', 'in_deg_centrality', 'out_deg_centrality', 'closeness_centrality', 'betweeness_centrality', 'eigenvector_centrality')
-cent_measure_used = 5
+cent_measure_used = 0
 
 # Split the data for the specified number of clients and servers
 if iid_type == 'iid':
